4a5d785116f92581242bb6f8c3ee372d1b2a27f7
juloelker <35081946+juloelker@users.noreply.github.com>
2021-03-11 10:32:44 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -9,20 +9,6 @@
 
 // alert(myNameIs());
 
-//Make it dependent on clicking the Save
-//button even if no text is entered.
-// Two issues:
-// 1. On page load, the alert is triggered immediately.
-// Removed the () when calling myName function.
-
-// 2. Using myName(fname
-// = "Julie", lname = "Oelker") directly in the
-// function parameters caused the first
-// parameter not to display in the alert,
-// got "Hi [onclickevent] Oelker!"
-// Fixed this by placing the values inside the
-// function.
-
 const btn = document.querySelector("#txt-btn");
 btn.addEventListener("click", myName);
 
